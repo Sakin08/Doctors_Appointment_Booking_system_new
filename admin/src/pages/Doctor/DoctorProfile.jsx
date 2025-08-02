d622943116afbcf77503acb2e05931c99d27668c
Sakin <sakin@Sakins-MacBook-Air.local>
2025-08-02 19:35:08 +0600

    updated

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -232,7 +232,7 @@ const DoctorProfile = () => {
 
                 <div>
                   <p className="mb-1 font-medium text-gray-700">Consultation Fee</p>
-                  <p className="px-3 py-2 border border-gray-300 rounded-md bg-gray-50">${doctorInfo.fees}</p>
+                  <p className="px-3 py-2 border border-gray-300 rounded-md bg-gray-50">Tk {doctorInfo.fees}</p>
                 </div>
               </div>
 
